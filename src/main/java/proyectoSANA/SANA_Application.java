a15b55c5083592403655a54536245eaaed20cb73
JaviDiaz99 <al388523@uji.es>
2021-04-09 02:10:16 +0200

    Actualizacion :)

--- src/main/java/proyectoSANA/SANA_Application.java
+++ src/main/java/proyectoSANA/SANA_Application.java
@@ -1,4 +1,23 @@
 package proyectoSANA;
 
-public class SANA_Application {
+import java.util.logging.Logger;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SANA_Application implements CommandLineRunner {
+    private static final Logger log = Logger.getLogger(SANA_Application.class.getName());
+
+    public static void main(String[] args) {
+        // Auto-configura l'aplicació
+        new SpringApplicationBuilder(SANA_Application.class).run(args);
+    }
+
+    // Funció principal
+    public void run(String... strings) throws Exception {
+        log.info("Ací va el meu codi");
+    }
 }
+
