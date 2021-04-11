8eabe70d6464b60bee967585157831cbee044a8f
MarcosJovani <marcos.jovani98@gmail.com>
2021-04-11 19:13:06 +0200

    Clases para la tabla Municipio

---
+++ src/main/java/proyectoSANA/controller/MunicipioController.java
@@ -0,0 +1,71 @@
+package proyectoSANA.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import proyectoSANA.dao.MunicipioDao;
+import proyectoSANA.model.Municipio;
+
+@Controller
+@RequestMapping("/municipio")
+public class MunicipioController {
+
+    private MunicipioDao municipioDao;
+
+    @Autowired
+    public void setMunicipioDao(MunicipioDao municipioDao) {
+        this.municipioDao=municipioDao;
+    }
+
+    // Operacions: Crear, llistar, actualitzar, esborrar
+    // ...
+    @RequestMapping(value="/add")
+    public String addMunicipio(Model model) {
+        model.addAttribute("municipio", new Municipio());
+        return "municipio/add";
+    }
+
+
+    @RequestMapping("/list")
+    public String listMunicipios(Model model) {
+        model.addAttribute("municipios", municipioDao.getMunicipios());
+        return "municipio/list";
+    }
+
+    @RequestMapping(value="/add", method= RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("municipio") Municipio municipio,
+                                   BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "municipio/add";
+        municipioDao.addMunicipio(municipio);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/update/{nom}", method = RequestMethod.GET)
+    public String editMunicipio(Model model, @PathVariable String nom) {
+        model.addAttribute("municipio", municipioDao.getMunicipio(nom));
+        return "municipio/update";
+    }
+
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(
+            @ModelAttribute("municipio") Municipio municipio,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "municipio/update";
+        municipioDao.updateMunicipio(municipio);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/delete/{nom}")
+    public String processDelete(@PathVariable String nom) {
+        municipioDao.deleteMunicipio(nom);
+        return "redirect:../list";
+    }
+
+}
---
+++ src/main/java/proyectoSANA/dao/MunicipioDao.java
@@ -0,0 +1,71 @@
+package proyectoSANA.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import proyectoSANA.model.Municipio;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository // En Spring els DAOs van anotats amb @Repository
+public class MunicipioDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    // Obté el jdbcTemplate a partir del Data Source
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    /* Afegeix el municipio a la base de dades */
+    public void addMunicipio(Municipio municipio) {
+        jdbcTemplate.update("INSERT INTO Municipio VALUES(?, ?, ?, ?, ?)",
+                municipio.getNom(), municipio.getTelefono(), municipio.getCp());
+    }
+
+    /* Esborra el nadador de la base de dades */
+    public void deleteMunicipio(String nom) {
+        jdbcTemplate.update("DELETE FROM Municipio WHERE nom = ?",
+                nom);
+    }
+
+    /* Esborra el nadador de la base de dades */
+    public void deleteMunicipio(Municipio municipio) {
+        jdbcTemplate.update("DELETE FROM Municipio WHERE nom = ?",
+                municipio.getNom());
+    }
+
+    /* Actualitza els atributs del nadador
+       (excepte el nom, que és la clau primària) */
+    public void updateMunicipio(Municipio municipio) {
+        jdbcTemplate.update("UPDATE Municipio SET telefono = ?, cp = ? WHERE nom = ?",
+                municipio.getTelefono(), municipio.getCp(), municipio.getNom());
+    }
+
+    /* Obté el municipi amb el nom donat. Torna null si no existeix. */
+        public Municipio getMunicipio(String nomMunicipio) {
+        try {
+            return jdbcTemplate.queryForObject(
+                    "SELECT * FROM Municipio WHERE nom =?",
+                    new MunicipioRowMapper(),
+                    nomMunicipio);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    /* Obté tots els municipis. Torna una llista buida si no n'hi ha cap. */
+    public List<Municipio> getMunicipios() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM Municipio", new MunicipioRowMapper());
+        }
+        catch(EmptyResultDataAccessException e) {
+            return new ArrayList<Municipio>();
+        }
+    }
+}
