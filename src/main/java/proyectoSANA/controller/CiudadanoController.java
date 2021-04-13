f03c49b60fb5374f0dc5f181f67c6eabbfb85c6a
JaviDiaz99 <al388523@uji.es>
2021-04-13 12:58:32 +0200

    ya esta

---
+++ src/main/java/proyectoSANA/controller/CiudadanoController.java
@@ -0,0 +1,4 @@
+package proyectoSANA.controller;
+
+public class CiudadanoController {
+}
---
+++ src/main/java/proyectoSANA/dao/CiudadanoRowMapper.java
@@ -0,0 +1,22 @@
+package proyectoSANA.dao;
+
+import proyectoSANA.model.Ciudadano;
+
+import org.springframework.jdbc.core.RowMapper;
+import proyectoSANA.model.Municipio;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public final class CiudadanoRowMapper  implements RowMapper<Ciudadano> {
+    public Ciudadano mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Ciudadano ciudadano = new Ciudadano();
+        ciudadano.setNombre(rs.getString("nombre"));
+        ciudadano.setDNI(rs.getInt("DNI"));
+        ciudadano.setEmail(rs.getString("email"));
+        ciudadano.setDireccion(rs.getString("direccion"));
+        ciudadano.setMunicipio(rs.getString("municipio"));
+        ciudadano.setPais(rs.getString("pais"));
+        return ciudadano;
+    }
+}
