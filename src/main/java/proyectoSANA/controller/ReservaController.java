9b77c62be9f88d49a4715f047f5d38b6489876f8
aroob330@gmail.com <aroob330@gmail.com>
2021-04-15 21:25:50 +0200

    Reserva

---
+++ src/main/java/proyectoSANA/controller/ReservaController.java
@@ -0,0 +1,71 @@
+package proyectoSANA.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import proyectoSANA.dao.ReservaDao;
+import proyectoSANA.model.Reserva;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/reserva")
+public class ReservaController {
+    private ReservaDao reservaDao;
+
+    @Autowired
+    public void setReservaDao(ReservaDao reservaDao) {
+        this.reservaDao=reservaDao;
+    }
+
+    // Operacions: Crear, llistar, actualitzar, esborrar
+    // ...
+    @RequestMapping(value="/add")
+    public String addReserva(Model model) {
+        model.addAttribute("reserva", new Reserva());
+        return "reserva/add";
+    }
+
+
+    @RequestMapping("/list")
+    public String listReserva(Model model) {
+        model.addAttribute("reservas", reservaDao.getReservas());
+        return "reserva/list";
+    }
+
+    @RequestMapping(value="/add", method= RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("reserva") Reserva reserva,
+                                   BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "reserva/add";
+        reservaDao.addReserva(reserva);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/update/{nombre}", method = RequestMethod.GET)
+    public String editReserva(Model model, @PathVariable int dniPersona, @PathVariable Date fecha) {
+        model.addAttribute("reserva", reservaDao.getReserva(dniPersona, fecha));
+        return "reserva/update";
+    }
+
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(
+            @ModelAttribute("reserva") Reserva reserva,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "reserva/update";
+        reservaDao.updateReserva(reserva);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value="/delete/{nombre}")
+    public String processDelete(@PathVariable String persona, @PathVariable Date fecha) {
+        reservaDao.deleteReserva(persona, fecha);
+        return "redirect:../list";
+    }
+}
