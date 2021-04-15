ca9e444deadbc4b568fc96063189533d546b61df
aroob330@gmail.com <aroob330@gmail.com>
2021-04-15 19:10:38 +0200

    Reserva

---
+++ src/main/java/proyectoSANA/dao/ReservaDao.java
@@ -0,0 +1,73 @@
+package proyectoSANA.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import proyectoSANA.model.Ciudadano;
+import proyectoSANA.model.Reserva;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Repository // En Spring els DAOs van anotats amb @Repository
+public class ReservaDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    // Obté el jdbcTemplate a partir del Data Source
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    /* Afegeix la reserva a la base de dades */
+    public void addReserva(Reserva reserva) {
+        jdbcTemplate.update("INSERT INTO Reserva VALUES(?, ?, ?, ?, ?, ?, ?, ?)",
+                reserva.getPersona(), reserva.getFecha(), reserva.getNumeroPersonas(), reserva.getQr(), reserva.getLimiteReserva(), reserva.getArea(), reserva.getZona(), reserva.getHorarioReserva());
+    }
+
+    /* Esborra la reserva de la base de dades */
+    public void deleteReserva(String persona, Date fecha) {
+        jdbcTemplate.update("DELETE FROM Reserva WHERE DNI = ? AND fecha = ?",
+                persona, fecha);
+    }
+
+    /* Esborra la reserva de la base de dades */
+    public void deleteCiudadano(Reserva reserva) {
+        jdbcTemplate.update("DELETE FROM Reserva WHERE DNI = ? AND fecha = ?",
+                reserva.getPersona(), reserva.getFecha());
+    }
+
+    /* Actualitza els atributs del nadador
+       (excepte el nom, que és la clau primària) */
+    public void updateReserva(Reserva reserva) {
+        jdbcTemplate.update("UPDATE Reserva SET numeroPersonas = ?, qr = ?, limiteReserva = ?, area = ?, zona = ?, horarioReserva = ? WHERE DNI = ? AND fecha = ?",
+                reserva.getNumeroPersonas(), reserva.getQr(), reserva.getLimiteReserva(), reserva.getArea(), reserva.getZona(), reserva.getHorarioReserva(), reserva.getPersona(), reserva.getFecha());
+    }
+
+    /* Obté la reserv amb el dni i  fecha donats. Torna null si no existeix. */
+    public Reserva getReserva(int dniPersona, Date fecha) {
+        try {
+            return jdbcTemplate.queryForObject(
+                    "SELECT * FROM Reserva WHERE DNI = ? AND fecha = ?",
+                    new ReservaRowMapper(),
+                    dniPersona, fecha);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    /* Obté tots els municipis. Torna una llista buida si no n'hi ha cap. */
+    public List<Reserva> getReservas() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM Reserva", new ReservaRowMapper());
+        }
+        catch(EmptyResultDataAccessException e) {
+            return new ArrayList<Reserva>();
+        }
+    }
+}
---
+++ src/main/java/proyectoSANA/dao/ReservaRowMapper.java
@@ -0,0 +1,23 @@
+package proyectoSANA.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import proyectoSANA.model.Reserva;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReservaRowMapper implements RowMapper<Reserva> {
+    public Reserva mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Reserva reserva = new Reserva();
+
+        reserva.setPersona(rs.getString("persona"));
+        reserva.setFecha(rs.getDate("fecha"));
+        reserva.setNumeroPersonas(rs.getInt("numeroPersonas"));
+        reserva.setQr(rs.getString("qr"));
+        reserva.setLimiteReserva(rs.getInt("limiteReserva"));
+        reserva.setArea(rs.getString("area"));
+        reserva.setZona(rs.getString("zona"));
+       // reserva.setHorarioReserva(rs.getString("horarioReserva"));
+        return reserva;
+    }
+}
