1a4e820e9341e440ca33b47b81e014451ecfaebb
juan <al316868@uji.es>
2021-04-17 12:33:06 +0200

    l

--- src/main/java/proyectoSANA/controller/AreaController.java
+++ src/main/java/proyectoSANA/controller/AreaController.java
@@ -9,7 +9,60 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import proyectoSANA.dao.AreaDao;
+import proyectoSANA.dao.MunicipioDao;
 import proyectoSANA.model.Area;
+import proyectoSANA.dao.AreaDAO;
+import proyectoSANA.model.Area;
+import proyectoSANA.model.Municipio;
 
+@Controller
+@RequestMapping("/area")
 public class AreaController {
+
+    private AreaDAO areaDAO;
+
+    @Autowired
+    public void setAreaDAO(AreaDao areaDao) {
+        this.areaDao = areaDao;
+    }
+
+    @RequestMapping(value="/add")
+    public String addArea(Model model) {
+        model.addAttribute("area", new Area());
+        return "area/add";
+    }
+
+    @RequestMapping("/list")
+    public String listMunicipios(Model model) {
+        model.addAttribute("municipios", municipioDao.getMunicipios());
+        return "municipio/list";
+    }
+
+    @RequestMapping(value="/add", method= RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("area") Area area,
+                                   BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "area/add";
+        areaDao.addArea(area);
+        return "redirect:list";
+    }
+    @RequestMapping(value="/update/{nombre}", method = RequestMethod.GET)
+    public String editArea(Model model, @PathVariable String nombre) {
+        model.addAttribute("area", areaDao.getArea(nombre));
+        return "area/update";
+    }
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(
+            @ModelAttribute("area") Area area,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "area/update";
+        areaDao.updateArea(area);
+        return "redirect:list";
+    }
+    @RequestMapping(value="/delete/{nombre}")
+    public String processDelete(@PathVariable String nombre) {
+        areaDao.deleteArea(nombre);
+        return "redirect:../list";
+    }
 }
