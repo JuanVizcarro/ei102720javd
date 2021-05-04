23c6fab583e037918faa7e1d4294de4ca118eb47
juan <al316868@uji.es>
2021-05-04 11:27:18 +0200

    Creado servicio

--- src/main/java/proyectoSANA/model/Servicio.java
+++ src/main/java/proyectoSANA/model/Servicio.java
@@ -1,4 +1,18 @@
 package proyectoSANA.model;
 
+import org.apache.tomcat.jni.Time;
+
 public class Servicio {
+    private String identificador;
+    private String nombre;
+    private String descripcion;
+    private String tipo;
+    private String area;
+    private int numero;
+    private Time fechaInicio;
+    private Time fechaFin;
+
+    public Servicio(){
+
+    }
 }
