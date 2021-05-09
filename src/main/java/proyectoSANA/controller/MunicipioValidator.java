36d5772cce901dcc7b3eaf1c1cc4835ae9d9d758
MarcosJovani <marcos.jovani98@gmail.com>
2021-05-09 11:30:05 +0200

    municipio actualizado

--- src/main/java/proyectoSANA/controller/MunicipioValidator.java
+++ src/main/java/proyectoSANA/controller/MunicipioValidator.java
@@ -15,15 +15,15 @@ public class MunicipioValidator implements Validator {
     public void validate(Object obj, Errors errors) {
         Municipio municipio = (Municipio) obj;
         if (municipio.getNombre().trim().equals(""))
-            errors.rejectValue("municipio", "obligatorio",
+            errors.rejectValue("nombre", "obligatorio",
                     "Este campo es obligatorio.");
 
         if (String.valueOf(municipio.getTlf()).length()!=9)
-            errors.rejectValue("tipoAcceso", "obligatorio",
+            errors.rejectValue("tlf", "obligatorio",
                     "Este campo debe tener 9 dígitos.");
 
         if (String.valueOf(municipio.getCp()).length()!=5)
-            errors.rejectValue("descripcion", "obligatorio",
+            errors.rejectValue("cp", "obligatorio",
                     "Este campo debe tener 5 dígitos.");
 
     }
