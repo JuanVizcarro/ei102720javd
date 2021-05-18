2f460a9b814706afdf0b6fa3e320a5ffcfd760e1
MarcosJovani <marcos.jovani98@gmail.com>
2021-05-18 12:01:46 +0200

    masInfo

--- src/main/java/proyectoSANA/controller/AreaController.java
+++ src/main/java/proyectoSANA/controller/AreaController.java
@@ -40,6 +40,7 @@ public class AreaController {
     @RequestMapping("/carrusel")
     public String carrusel(Model model) {
         List<Area> areas = areaDao.getAreas();
+        model.addAttribute("areas", areas);
         for (Area area:areas){
             imagenes.add(area.getImagen());
         }
