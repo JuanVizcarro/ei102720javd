ea1eb41252ce621213cd2719e83900f2a436311c
aroob330@gmail.com <al3861>
2021-05-22 14:45:47 +0200

    nav

--- src/main/java/proyectoSANA/dao/GestorMedioambientalDao.java
+++ src/main/java/proyectoSANA/dao/GestorMedioambientalDao.java
@@ -11,6 +11,7 @@ import proyectoSANA.model.Municipio;
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
+@Repository
 
 public class GestorMedioambientalDao {
     private JdbcTemplate jdbcTemplate;
